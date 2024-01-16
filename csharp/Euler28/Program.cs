fc632067d08caac5738512146fd6cb8a83efd493
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 11:19:41 -0600

    feat: add 28

---
+++ csharp/Euler28/Program.cs
@@ -0,0 +1,15 @@
+﻿var sum = 1;
+var last = 1;
+var delta = 2;
+
+for (var i = 0; i < 500; i++)
+{
+    for (var j = 0; j < 4; j++)
+    {
+        last += delta;
+        sum += last;
+    }
+    delta += 2;
+}
+
+Console.WriteLine(sum);
