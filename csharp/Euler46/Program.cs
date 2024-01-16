8e764997392e4a491e5a200dd9c8584966dd0da3
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 15:56:57 -0600

    feat: add 46

---
+++ csharp/Euler46/Program.cs
@@ -0,0 +1,29 @@
+﻿int result = 5;
+int f = 1;
+var primes = new HashSet<int>();
+
+while (true)
+{
+    if (!primes.Any(p => result % p == 0))
+        primes.Add(result);
+    else
+    {
+        bool found = false;
+        for (int i = 1; i < result; i++)
+        {
+            if (primes.Contains(result - 2 * i * i))
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            break;
+    }
+
+    result += 3 - f;
+    f = -f;
+}
+
+Console.WriteLine(result);
