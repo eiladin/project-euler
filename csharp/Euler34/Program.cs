9ff6cc3ea217f9b8ba27cef0b76694a75fac60fe
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 13:40:18 -0600

    feat: add 34

---
+++ csharp/Euler34/Program.cs
@@ -0,0 +1,21 @@
+﻿long result = 0;
+
+for (var i = 3; i <= 1_000_000; i++)
+{
+    var sumOfDigitFactorial = GetDigits(i).Sum(n => Factorial(n));
+    if (sumOfDigitFactorial == i)
+        result += i;
+}
+
+Console.WriteLine(result);
+
+static long Factorial(long n) => n == 0 ? 1 : n * Factorial(n - 1);
+
+static IEnumerable<long> GetDigits(long n)
+{
+    while (n > 0)
+    {
+        yield return n % 10;
+        n /= 10;
+    }
+}
