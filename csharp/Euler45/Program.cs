d38ca7353dbb8b66bb0e04303c95a38b1d911220
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 15:21:55 -0600

    feat: add 45

---
+++ csharp/Euler45/Program.cs
@@ -0,0 +1,14 @@
+﻿long h = 144L, p = 166L;
+long hex, penta = 0L;
+do
+{
+    hex = Hexagonal(h++);
+    while (penta < hex)
+        penta = Pentagonal(p++);
+} while (hex != penta);
+
+Console.WriteLine(hex);
+
+static long Hexagonal(long n) => n * (2 * n - 1);
+
+static long Pentagonal(long n) => n * (3 * n - 1) / 2;
