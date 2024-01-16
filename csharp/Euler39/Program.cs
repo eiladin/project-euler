0328cdcc6b4f551163945c1c6d726cdcd5f2a1ee
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 14:06:18 -0600

    feat: add 39

---
+++ csharp/Euler39/Program.cs
@@ -0,0 +1,13 @@
+﻿var max = Enumerable.Range(1, 1000).Select(p => (p, GetSidesForPermimeter(p).Count())).OrderByDescending(x => x.Item2).First().p;
+Console.WriteLine(max);
+
+static IEnumerable<(int a, int b, int c)> GetSidesForPermimeter(int p)
+{
+    for (var a = 1; a < p; a++)
+        for (var b = 1; b < p; b++)
+        {
+            var c = p - a - b;
+            if (c > 0 && a * a + b * b == c * c)
+                yield return (a, b, c);
+        }
+}
