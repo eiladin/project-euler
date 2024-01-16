7984bfcb3ea613a4b1964048fb32f9da34b2c916
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 14:01:05 -0600

    feat: add 38

---
+++ csharp/Euler38/Program.cs
@@ -0,0 +1,19 @@
+﻿var max = 0;
+
+for (var i = 1; i < 10000; i++)
+{
+    var concatProduct = string.Empty;
+
+    for (int j = 1; j < 10; j++)
+    {
+        concatProduct += (i * j).ToString();
+        if (concatProduct.Length > 9)
+            break;
+
+        if (IsPandigital(concatProduct) && int.Parse(concatProduct) > max)
+            max = int.Parse(concatProduct);
+    }
+}
+Console.Write(max);
+
+static bool IsPandigital(string s) => s.ToCharArray().Distinct().Count() == 9 && !s.Contains('0');
