5777eefe9c9b2aa493a57ef7087e350d40b3da5b
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 14:16:43 -0600

    feat: add 40

---
+++ csharp/Euler40/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+var i = 1;
+StringBuilder sb = new();
+while (sb.Length < 1_000_000)
+    sb.Append(i++);
+
+var result = 1;
+var s = sb.ToString();
+for (var j = 1; j <= 1_000_000; j *= 10)
+    result *= int.Parse(s[j - 1].ToString());
+
+Console.WriteLine(result);
