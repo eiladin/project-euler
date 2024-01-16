277faaac8e38ddc67bc71b75b70c1ebcd2442c54
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 11:42:03 -0600

    feat: add 31

---
+++ csharp/Euler31/Program.cs
@@ -0,0 +1,17 @@
+﻿var result = Calc(0, 200);
+Console.WriteLine(result);
+
+static int[] Values() => [200, 100, 50, 20, 10, 5, 2, 1];
+
+static int Calc(int index, int r)
+{
+    int count = 0;
+    if (r == 0 || index == Values().Length - 1)
+    {
+        count++;
+        return count;
+    }
+    for (int i = 0; i <= r / Values()[index]; i++)
+        count += Calc(index + 1, r - i * Values()[index]);
+    return count;
+}
