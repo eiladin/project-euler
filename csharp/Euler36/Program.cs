c7a99f220b471d5791b3f96943c02a9132b0861e
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 13:48:47 -0600

    feat: add 36

---
+++ csharp/Euler36/Program.cs
@@ -0,0 +1,11 @@
+﻿int count = 0;
+
+for (var i = 1; i < 1_000_000; i++)
+    if (IsPalindrome(i.ToString()) && IsPalindrome(ToBinary(i)))
+        count += i;
+
+Console.WriteLine(count);
+
+static string ToBinary(int n) => Convert.ToString(n, 2);
+
+static bool IsPalindrome(string s) => s.Reverse().SequenceEqual(s);
