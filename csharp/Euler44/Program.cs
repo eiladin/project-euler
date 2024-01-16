74917160d9c4d354cdeb9876036236ef2fd72619
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 15:07:55 -0600

    feat: add 44

---
+++ csharp/Euler44/Program.cs
@@ -0,0 +1,25 @@
+﻿
+bool found = false;
+int n = 2;
+while (!found)
+{
+    for (int i = n - 1; i > 0; i--)
+    {
+        int number1 = GetPentagonNumber(n);
+        int number2 = GetPentagonNumber(n - i);
+        if (IsPentagonal(number1 + number2) && IsPentagonal(number1 - number2))
+        {
+            Console.WriteLine(Math.Abs(GetPentagonNumber(n - i) - GetPentagonNumber(n)));
+            return;
+        }
+    }
+    n++;
+}
+
+static bool IsPentagonal(int num)
+{
+    var result = (Math.Sqrt(24 * num + 1) + 1) / 6;
+    return Math.Floor(result) == result;
+}
+
+static int GetPentagonNumber(int num) => num * (3 * num - 1) / 2;
