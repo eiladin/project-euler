25371f190a695b99bfb9ce4af3d6c1fca2043442
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 11:23:23 -0600

    feat: add 29

---
+++ csharp/Euler29/Program.cs
@@ -0,0 +1,6 @@
+﻿IEnumerable<double> list = Enumerable.Range(2, 99)
+    .SelectMany(x => Enumerable.Range(2, 99)
+        .Select(y => Math.Pow(x, y)))
+    .Distinct();
+
+Console.WriteLine(list.Count());
