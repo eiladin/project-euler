ee80a54db812ec74ddba9353cda47e55f7d14367
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-16 14:36:51 -0600

    feat: add 42

---
+++ csharp/Euler42/Program.cs
@@ -0,0 +1,21 @@
+﻿
+List<string> words = File.ReadAllText("input.txt").Split(',').Select(w => w.Trim('"')).ToList();
+
+Console.WriteLine(words.Count(w => IsTriangleWord(w)));
+
+static bool IsTriangleWord(string s)
+{
+    int sum = 0;
+    foreach (char c in s)
+        sum += c - 'A' + 1;
+    int n = 1;
+    while (true)
+    {
+        int t = n * (n + 1) / 2;
+        if (t == sum)
+            return true;
+        else if (t > sum)
+            return false;
+        n++;
+    }
+}
