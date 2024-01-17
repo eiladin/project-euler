67876f2f39c25b9780b725b2f9fe6fe2b8ba9e6b
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-17 17:04:01 -0600

    feat: add 56

---
+++ csharp/Euler56/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Numerics;
+
+var max = Enumerable.Range(1, 100)
+                    .SelectMany(a => Enumerable.Range(1, 100).Select(b => BigInteger.Pow(a, b).Digits().Sum()))
+                    .Max();
+
+Console.WriteLine(max);
+
+static class Extensions
+{
+    static public IEnumerable<int> Digits(this BigInteger n)
+    {
+        while (n > 0)
+        {
+            yield return (int)(n % 10);
+            n /= 10;
+        }
+    }
+}
