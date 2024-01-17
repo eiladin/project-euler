15b21de3590dad913be34c9c2e798d0175ad7805
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-17 12:05:35 -0600

    feat: add 52

---
+++ csharp/Euler52/Program.cs
@@ -0,0 +1,18 @@
+﻿var result = Enumerable.Range(1, 1000000).First(
+    num => Enumerable.Range(2, 5).All(
+        multiplier => GetDigits(num, multiplier).All(
+            d => GetDigits(num, 1).Contains(d)
+        )
+    )
+);
+Console.WriteLine(result);
+
+static IEnumerable<int> GetDigits(int number, int multiplier)
+{
+    number *= multiplier;
+    while (number > 0)
+    {
+        yield return number % 10;
+        number /= 10;
+    }
+}
