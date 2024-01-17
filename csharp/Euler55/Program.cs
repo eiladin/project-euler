3ca35e89a3cc421303456bd85ad2756d947e4049
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-17 16:50:42 -0600

    feat: add 55

---
+++ csharp/Euler55/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+var total = 0;
+for (var i = 10; i < 10_000; i++)
+    total += IsLychrelNumber(i) ? 1 : 0;
+
+Console.WriteLine(total);
+
+static bool IsLychrelNumber(int number)
+{
+    var num = number.ToString();
+    for (var i = 0; i < 50; i++)
+    {
+        num = (BigInteger.Parse(num) + BigInteger.Parse(new string(num.Reverse().ToArray()))).ToString();
+        if (IsPalindrome(num))
+            return false;
+    }
+    return true;
+}
+
+static bool IsPalindrome(string s) => s.Reverse().SequenceEqual(s.ToCharArray());
