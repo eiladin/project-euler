ce3ab9b2c2174c9cb232574ca99526e792207f8f
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-18 11:42:41 -0600

    feat: add 61

---
+++ csharp/Euler61/Program.cs
@@ -0,0 +1,59 @@
+﻿
+var numbers = new Dictionary<(int, int), HashSet<int>>();
+for (var i = 0; i < 9; i++)
+    for (var j = 0; j < 100; j++)
+        numbers[(i, j)] = [];
+
+for (var sides = 3; sides <= 8; sides++)
+{
+    for (var n = 1; ; n++)
+    {
+        var num = GetNumber(sides, n);
+        if (num >= 10000)
+            break;
+        if (num >= 1000)
+            numbers[(sides, num / 100)].Add(num);
+    }
+}
+
+Console.WriteLine(Calculate());
+
+int Calculate()
+{
+    for (var i = 10; i < 100; i++)
+    {
+        foreach (var num in numbers[(3, i)])
+        {
+            var result = FindSolutionSum(num, num, 1 << 3, num);
+            if (result.HasValue)
+                return result.Value;
+        }
+    }
+    return -1;
+}
+
+int? FindSolutionSum(int begin, int current, int sidesUsed, int sum)
+{
+    if (sidesUsed == 504)
+    {
+        if (current % 100 == begin / 100)
+            return sum;
+    }
+    else
+    {
+        for (var sides = 4; sides <= 8; sides++)
+        {
+            if (((sidesUsed >>> sides) & 1) != 0)
+                continue;
+            foreach (var num in numbers[(sides, current % 100)])
+            {
+                var result = FindSolutionSum(begin, num, sidesUsed | (1 << sides), sum + num);
+                if (result.HasValue)
+                    return result.Value;
+            }
+        }
+    }
+    return null;
+}
+
+static int GetNumber(int sides, int n) => n * ((sides - 2) * n - (sides - 4)) / 2;
