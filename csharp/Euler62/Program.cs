5bdd419d637cb6bab28abf661a3e97f9f35b7f66
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-18 11:58:34 -0600

    feat: add 62

---
+++ csharp/Euler62/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+var digits = string.Empty;
+BigInteger minCube = -1;
+var n = 100;
+var d = 5;
+var cubes = new Dictionary<string, List<BigInteger>>();
+
+while (minCube == -1 || digits.Length < minCube.ToString().Length)
+{
+    var c = BigInteger.Pow(n, 3);
+    digits = c.ToString().OrderBy(x => x).Aggregate(string.Empty, (acc, x) => acc + x);
+    if (cubes.TryGetValue(digits, out List<BigInteger>? value))
+    {
+        value.Add(c);
+        if (value.Count == d)
+            minCube = value.Min();
+    }
+    else
+        cubes.Add(digits, [c]);
+    n++;
+}
+
+Console.WriteLine(minCube);
