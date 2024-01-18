5673a7e3c454c2e768d1495c5f4da4eeafb4d17d
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-17 18:09:51 -0600

    feat: add 57

---
+++ csharp/Euler57/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+BigInteger numerator = 1;
+BigInteger denominator = 1;
+var count = 0;
+for (var i = 1; i <= 1000; i++)
+{
+    var a = numerator;
+    var b = denominator;
+    numerator = 2 * b + a;
+    denominator += a;
+    if (numerator.ToString().Length - denominator.ToString().Length > 0)
+        count++;
+}
+Console.WriteLine(count);
