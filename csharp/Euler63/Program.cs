a0244986d5e64ffccaa3b098a5fdb2b9d51660bc
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-18 13:24:59 -0600

    feat: add 63

---
+++ csharp/Euler63/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+List<BigInteger> list = [];
+for (BigInteger num = 1; num < 10; num++)
+{
+    for (int power = 1; power <= 25; power++)
+    {
+        BigInteger result = BigInteger.Pow(num, power);
+        var len = result.ToString().Length;
+        if (len > power)
+            break;
+        if (len == power)
+            list.Add(result);
+    }
+}
+Console.WriteLine(list.Count);
