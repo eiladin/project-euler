ed71b2300d3887b86fdde05466ee0629f17df716
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-18 21:04:42 -0600

    feat: add 67

---
+++ csharp/Euler67/Program.cs
@@ -0,0 +1,9 @@
+﻿var triangle = File.ReadAllLines("input.txt")
+    .Select(line => line.Split(' ').Select(int.Parse).ToArray())
+    .ToArray();
+
+for (var i = triangle.Length - 1; i > 0; i--)
+    for (var j = 0; j < triangle[i].Length - 1; j++)
+        triangle[i - 1][j] += Math.Max(triangle[i][j], triangle[i][j + 1]);
+
+Console.WriteLine(triangle[0][0]);
