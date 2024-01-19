6c53a046202a257a5e2c8a1aedae2ec96bb48a83
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-18 22:43:43 -0600

    feat: add 74

---
+++ csharp/Euler74/Program.cs
@@ -0,0 +1,22 @@
+﻿using Euler;
+
+var count = 0;
+
+for (long num = 2; num < 1_000_000; num++)
+{
+    var chain = new List<long> { num };
+    var next = num;
+    while (true)
+    {
+        next = GetNext(next);
+        if (chain.Contains(next))
+            break;
+        chain.Add(next);
+    }
+    if (chain.Count == 60)
+        count++;
+}
+
+Console.WriteLine(count);
+
+static long GetNext(long num) => Numerics.GetDigits(num).Select(d => Numerics.Factorial(d)).Sum();
