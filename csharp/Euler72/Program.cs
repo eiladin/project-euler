fbd22d2a51d7f77fcbea68f32b8060992e457113
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-18 22:33:41 -0600

    feat: add 72

---
+++ csharp/Euler72/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+using Euler;
+
+int number = 1000000;
+BigInteger sum = 1;
+
+var tots = GenerateTotients(number);
+
+foreach (var tot in tots)
+    sum += tot.Value;
+
+Console.WriteLine(sum - 2);
+
+static Dictionary<int, int> GenerateTotients(int n)
+{
+    var totients = new Dictionary<int, int> { { 0, 0 } };
+    List<List<int>> primeFactors = GeneratePrimeFactors(n);
+    for (int i = 1; i < primeFactors.Count; i++)
+    {
+        double totient = i;
+        foreach (int j in primeFactors[i])
+            totient *= 1 - 1 / (double)j;
+        totients.Add(i, (int)totient);
+    }
+    return totients;
+}
+
+static List<List<int>> GeneratePrimeFactors(int n)
+{
+    var primes = Primes.Get(n);
+    var primeFactors = new List<List<int>>
+    {
+        new() { 0 }
+    };
+    for (int i = 1; i <= n; i++)
+        primeFactors.Add([]);
+    if (n == 1) return primeFactors;
+    foreach (int i in primes.Select(v => (int)v))
+        for (int j = i; j <= n; j += i)
+            primeFactors[j].Add(i);
+    return primeFactors;
+}
