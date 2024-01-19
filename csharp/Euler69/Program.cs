769249c6f9f78a98d92e3a05e091d59eb9420294
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-18 22:05:48 -0600

    feat: add 69

---
+++ csharp/Euler69/Program.cs
@@ -0,0 +1,33 @@
+﻿double maxRatio = 0;
+int maxN = 0;
+
+for (var i = 2; i <= 1000000; i++)
+{
+    var totient = EulerTotient(i);
+    var ratio = (double)i / totient;
+    if (ratio > maxRatio)
+    {
+        maxRatio = ratio;
+        maxN = i;
+    }
+}
+
+Console.WriteLine(maxN);
+
+
+static int EulerTotient(int n)
+{
+    int result = n;
+    for (int p = 2; p * p <= n; p++)
+        if (n % p == 0)
+        {
+            while (n % p == 0)
+                n /= p;
+            result -= result / p;
+        }
+
+    if (n > 1)
+        result -= result / n;
+
+    return result;
+}
