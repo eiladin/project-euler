f8789deb5588caf464787c1028585edeefee2d83
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-20 20:08:42 -0600

    feat: add 79

---
+++ csharp/Euler79/Program.cs
@@ -0,0 +1,14 @@
+﻿var attempts = File.ReadAllLines("input.txt");
+var digits = attempts.SelectMany(x => x).Distinct().ToArray();
+var graph = new Dictionary<char, HashSet<char>>();
+foreach (var digit in digits)
+    graph[digit] = [];
+
+foreach (var attempt in attempts)
+{
+    graph[attempt[0]].Add(attempt[1]);
+    graph[attempt[0]].Add(attempt[2]);
+    graph[attempt[1]].Add(attempt[2]);
+}
+var result = string.Join("", graph.Keys.OrderByDescending(x => graph[x].Count).ToArray());
+Console.WriteLine(result);
