7efb9a5c7de4d5d834ba6ed319c144f830ae39a9
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-22 10:31:04 -0600

    feat: add 89

---
+++ csharp/Euler89/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var lines = File.ReadAllLines("input.txt");
+var saved = lines.Select(l => l.Length - MinimizeRomanNumerals(l).Length).Sum();
+Console.WriteLine(saved);
+
+static string MinimizeRomanNumerals(string input) =>
+    new string(input.ToCharArray())
+        .Replace("DCCCC", "CM")
+        .Replace("CCCC", "CD")
+        .Replace("LXXXX", "XC")
+        .Replace("XXXX", "XL")
+        .Replace("VIIII", "IX")
+        .Replace("IIII", "IV");
