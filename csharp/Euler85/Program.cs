6ecbd083db7701d867c31042524a202d2a35e2e0
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-21 21:14:14 -0600

    feat: add 85

---
+++ csharp/Euler85/Program.cs
@@ -0,0 +1,20 @@
+﻿var target = 2_000_000;
+var bestDist = 2_000_000;
+var result = 0;
+
+for (var i = 1; i < 2000; i++)
+{
+    var n = (int)Math.Floor(Math.Sqrt(1 + 16 * target / (i * i + i) - 1.0) / 2.0);
+    for (var j = 0; j < 2; j++)
+    {
+        var sum = n * (i + 1) * i * (n + 1) / 4;
+        var dist = Math.Abs(sum - target);
+        if (dist < bestDist)
+        {
+            bestDist = dist;
+            result = n * i;
+        }
+        n++;
+    }
+}
+Console.WriteLine(result);
