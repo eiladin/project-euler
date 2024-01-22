4f601c1f0c4942e204b70b40647b75098743960d
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-21 21:56:30 -0600

    feat: add 87

---
+++ csharp/Euler87/Program.cs
@@ -0,0 +1,10 @@
+﻿using Euler;
+
+var primes = Primes.Get((int)Math.Sqrt(50_000_000));
+var result = Enumerable.Range(0, primes.Count)
+                       .SelectMany(a => Enumerable.Range(0, primes.Count)
+                            .SelectMany(b => Enumerable.Range(0, primes.Count)
+                                    .Select(c => primes[a] * primes[a] + primes[b] * primes[b] * primes[b] + primes[c] * primes[c] * primes[c] * primes[c])
+                                    .TakeWhile(x => x < 50_000_000).ToList()));
+
+Console.WriteLine(result.Distinct().Count());
