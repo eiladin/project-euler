d4eed58856f7ae5b97087a1089cd3be70d4ccef5
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-23 16:29:49 -0600

    feat: add 94

---
+++ csharp/Euler94/Program.cs
@@ -0,0 +1,14 @@
+﻿var sum = 0;
+var s = 5;
+var t = -1;
+var prev = 1;
+while (s < 333_333_334)
+{
+    sum += 3 * s - t;
+    var p = s;
+    s = 4 * s - prev + 2 * t;
+    t *= -1;
+    prev = p;
+}
+
+Console.WriteLine(sum);
