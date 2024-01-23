2589b0537db9b8b346dd838dfdd621ec121e5f18
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-23 09:20:40 -0600

    feat: add 91 and 92

---
+++ csharp/Euler91/Program.cs
@@ -0,0 +1,32 @@
+﻿var limit = 50;
+
+var triangles = new List<(int, int, int, int)>();
+
+for (var x1 = 0; x1 <= limit; x1++)
+    for (var y1 = 0; y1 <= limit; y1++)
+        for (var x0 = 0; x0 <= limit; x0++)
+            for (var y0 = 0; y0 <= limit; y0++)
+            {
+                if (IsValidRightTriangle(x0, y0, x1, y1) && !triangles.Contains((x0, y0, x1, y1)) && !triangles.Contains((x1, y1, x0, y0)))
+                    triangles.Add((x0, y0, x1, y1));
+            }
+
+Console.WriteLine(triangles.Count);
+
+static bool IsValidRightTriangle(int x0, int y0, int x1, int y1)
+{
+    if ((x0 == 0 && y0 == 0) || (x1 == 0 && y1 == 0))
+        return false;
+
+    if ((x0 == 0 && x1 == 0) || (y0 == 0 && y1 == 0))
+        return false;
+
+    if (x0 == x1 && y0 == y1)
+        return false;
+
+    var a = x0 * x0 + y0 * y0;
+    var b = x1 * x1 + y1 * y1;
+    var c = (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0);
+
+    return a + b == c || a + c == b || b + c == a;
+}
---
+++ csharp/Euler92/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Euler;
+
+Stopwatch sw = new();
+sw.Start();
+
+var nums = new Dictionary<int, bool>()
+{
+    { 1, false },
+    { 89, true }
+};
+
+for (var i = 2; i < 10_000_000; i++)
+{
+    var num = i;
+    while (num != 1 && num != 89)
+    {
+        num = Numerics.GetDigits(num).Sum(x => x * x);
+        if (nums.TryGetValue(num, out bool value))
+            num = value ? 89 : 1;
+    }
+    nums.TryAdd(i, num == 89);
+}
+
+Console.WriteLine(nums.Count(x => x.Value));
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds + "ms");
