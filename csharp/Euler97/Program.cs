4205736f62ab69724677d8687c6d8e2016f9a40a
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-24 11:20:14 -0600

    feat: update 97

--- csharp/Euler97/Program.cs
+++ csharp/Euler97/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-
-var power = 2L;
+﻿var power = 2L;
 for (var n = 2; n <= 7830457; n++)
-    power = power * 2 % 10000000000;
-Console.WriteLine((28433 * power + 1) % 10000000000);
+    power = power * 2 % 10_000_000_000;
+Console.WriteLine((28433 * power + 1) % 10_000_000_000);
