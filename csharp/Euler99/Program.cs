0d067afbec611387da1e45649fac32e7a2f6c33e
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-24 13:21:46 -0600

    feat: add 99

---
+++ csharp/Euler99/Program.cs
@@ -0,0 +1,4 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var numbers = lines.Select(line => line.Split(',')).Select((pair, i) => new { Index = i + 1, Value = long.Parse(pair[1]) * Math.Log(long.Parse(pair[0])) });
+var max = numbers.MaxBy(n => n.Value)!.Index;
+Console.WriteLine(max);
