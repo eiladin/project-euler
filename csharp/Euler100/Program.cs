5e7bc09dd2d4df72ec007bad692c1efe763da696
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-24 13:29:05 -0600

    feat: add 100

---
+++ csharp/Euler100/Program.cs
@@ -0,0 +1,12 @@
+﻿var blueDiscs = 15L;
+var totalDiscs = 21L;
+
+while (totalDiscs < 1_000_000_000_000)
+{
+    var newBlueDiscs = 3 * blueDiscs + 2 * totalDiscs - 2;
+    var newTotalDiscs = 4 * blueDiscs + 3 * totalDiscs - 3;
+    blueDiscs = newBlueDiscs;
+    totalDiscs = newTotalDiscs;
+}
+
+Console.WriteLine(blueDiscs);
