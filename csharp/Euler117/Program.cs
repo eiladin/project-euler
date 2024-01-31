10c6859b139a3f2e167337dc78921f0765a48204
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-31 09:38:52 -0600

    feat: add 117

---
+++ csharp/Euler117/Program.cs
@@ -0,0 +1,31 @@
+﻿Dictionary<(int, int[]), long> memo = [];
+
+var totalLen = 50;
+
+var blockSize = new[] { 1, 2, 3, 4 };
+var result = 0L;
+result = Divide(totalLen, blockSize).Sum();
+Console.WriteLine(result);
+
+IEnumerable<long> Divide(int totalLen, int[] blockSize)
+{
+    if (totalLen < 0)
+        yield return 0;
+    else if (totalLen == 0)
+        yield return 1;
+    else
+    {
+        var key = (totalLen, blockSize);
+        if (memo.TryGetValue(key, out long value))
+            yield return value;
+        else
+        {
+            foreach (var block in blockSize)
+            {
+                var s = Divide(totalLen - block, blockSize).Sum();
+                memo[(totalLen - block, blockSize)] = s;
+                yield return s;
+            }
+        }
+    }
+}
