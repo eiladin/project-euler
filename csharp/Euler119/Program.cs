6943d8709c675596d49de787911a85152a716ffd
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-31 13:36:09 -0600

    feat: update 119

---
+++ csharp/Euler119/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+using Euler;
+
+var results = Enumerable.Range(2, 2000)
+          .SelectMany(i => Enumerable.Range(2, 8).Select(power => (i, power, result: BigInteger.Pow(i, power))))
+          .Where(n => Numerics.GetDigits(n.result).Sum() == n.i)
+          .ToList();
+
+var result = results.OrderBy(x => x.result).Skip(29).First().result;
+Console.WriteLine(result);
