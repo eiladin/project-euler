b7012362f0e5e9cd66357ad1b1830e32394e02aa
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-31 12:02:13 -0600

    feat: add 118

---
+++ csharp/Euler118/Program.cs
@@ -0,0 +1,30 @@
+﻿using Euler;
+
+List<int> digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var permutations = Collections.GetPermutations(digits).Where(p => p.Last() % 2 != 0 || p.Count > 1 || p.Last() != 2);
+var solutions = permutations.SelectMany(p => Search(p, []).ToList());
+Console.WriteLine(solutions.Count());
+
+static IEnumerable<List<int>> Search(List<int> digits, List<int> merged, int firstPos = 0)
+{
+    if (firstPos == digits.Count)
+        yield return new List<int>(merged);
+
+    int current = 0;
+    while (firstPos < digits.Count)
+    {
+        current *= 10;
+        current += digits[firstPos++];
+
+        if (merged.Count > 0 && current < merged.Last())
+            continue;
+
+        if (Primes.IsPrime(current))
+        {
+            merged.Add(current);
+            foreach (var x in Search(digits, merged, firstPos))
+                yield return x;
+            merged.RemoveAt(merged.Count - 1);
+        }
+    }
+}
--- csharp/Utils/Primes.cs
+++ csharp/Utils/Primes.cs
@@ -38,7 +38,11 @@ public class Primes
 
     public static bool IsPrime(int n)
     {
-        for (int i = 2; i * i <= n; ++i)
+        if (n == 1)
+            return false;
+        if ((n & 1) == 0)
+            return n == 2;
+        for (int i = 3; i * i <= n; i += 2)
             if (n % i == 0)
                 return false;
         return true;
@@ -46,7 +50,11 @@ public class Primes
 
     public static bool IsPrime(long n)
     {
-        for (long i = 2; i * i <= n; ++i)
+        if (n == 1)
+            return false;
+        if ((n & 1) == 0)
+            return n == 2;
+        for (int i = 3; i * i <= n; i += 2)
             if (n % i == 0)
                 return false;
         return true;
