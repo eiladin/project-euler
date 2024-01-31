907d371beade82057f8a2da61cbfeb25fe752b64
Khan, Sami <Sami.Khan@tylertech.com>
2024-01-31 15:57:47 -0600

    feat: add 123

---
+++ csharp/Euler123/Program.cs
@@ -0,0 +1,14 @@
+﻿using Euler;
+
+var primeSieve = Primes.Sieve(1_000_000);
+var primes = primeSieve.Select((n, idx) => (n, idx)).Where(n => n.n).Select(n => (long)n.idx).ToList();
+var n = 7037;
+
+while (true)
+{
+    var r = 2 * n * primes[n - 1];
+    if (r > 10_000_000_000)
+        break;
+    n += 2;
+}
+Console.WriteLine(n);
