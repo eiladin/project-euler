ea96cc6cfa8b3314149dd069a87e1b8297cdcc14
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-01 13:26:19 -0600

    feat: add 128

---
+++ csharp/Euler128/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using Euler;
+
+var target = 2000;
+var count = 2;
+var result = BigInteger.Zero;
+for (int ring = 2; result == 0; ring++)
+{
+    if (new[] { ring * 6 - 1, ring * 6 + 1, ring * 12 + 5 }.All(Primes.IsPrime))
+    {
+        count += 1;
+        if (count == target)
+        {
+            result = (BigInteger)ring * (ring - 1) * 3 + 2;
+            break;
+        }
+    }
+    if (new[] { ring * 6 - 1, ring * 6 + 5, ring * 12 - 7 }.All(Primes.IsPrime))
+    {
+        count += 1;
+        if (count == target)
+        {
+            result = (BigInteger)ring * (ring + 1) * 3 + 1;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(result);
