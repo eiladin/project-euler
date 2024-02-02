70e322f7a4fbba3113a0d1147ad8db0b7c1e7e28
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-02 16:55:01 -0600

    feat: add 136

---
+++ csharp/Euler136/Program.cs
@@ -0,0 +1,14 @@
+﻿var limit = 50_000_000;
+var solutions = new int[limit];
+for (var m = 1; m < limit * 2; m++)
+{
+    for (var k = m / 5 + 1; k < (m + 1) / 2; k++)
+    {
+        var temp = (m - k) * (k * 5 - m);
+        if (temp >= limit)
+            break;
+        solutions[temp]++;
+    }
+}
+
+Console.WriteLine(solutions.Count(x => x == 1));
