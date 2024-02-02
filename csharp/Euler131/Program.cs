edbcb1a05c97280877d10226e1afec59b1cef30f
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-02 10:05:31 -0600

    feat: add 131

---
+++ csharp/Euler131/Program.cs
@@ -0,0 +1,11 @@
+﻿using Euler;
+
+int result = 0;
+for (int i = 1; ; i++)
+{
+    var n = (i + 1) * (i + 1) * (i + 1) - i * i * i;
+    if (n > 1_000_000)
+        break;
+    result += Primes.IsPrime(n) ? 1 : 0;
+}
+Console.WriteLine(result);
