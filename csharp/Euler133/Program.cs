4ef13f1726970d65bd632d7eeb48999fbe110895
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-02 10:20:49 -0600

    feat: add 133

---
+++ csharp/Euler133/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+using Euler;
+
+var result = 0L;
+var primes = Primes.Get(100_000);
+BigInteger k = BigInteger.Pow(10, 16);
+
+for (var i = 0; i < primes.Count; i++)
+{
+    if (BigInteger.ModPow(10, k, 9 * primes[i]) != 1)
+        result += primes[i];
+}
+Console.WriteLine(result);
