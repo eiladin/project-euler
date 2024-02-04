a2283da0dd6e739d2894c2e6e85177862b383c05
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-04 13:44:01 -0600

    feat: add 139

---
+++ csharp/Euler139/Program.cs
@@ -0,0 +1,20 @@
+﻿using Euler;
+
+var limit = 100_000_000;
+
+var result = 0;
+for (var m = 2; m < Math.Sqrt(limit / 2); m++)
+{
+    for (var n = 1; n < m; n++)
+    {
+        if ((m - n) % 2 == 1 && Numerics.Gcd(m, n) == 1)
+        {
+            var a = m * m - n * n;
+            var b = 2 * m * n;
+            var c = m * m + n * n;
+            if (c % (b - a) == 0)
+                result += limit / (a + b + c);
+        }
+    }
+}
+Console.WriteLine(result);
