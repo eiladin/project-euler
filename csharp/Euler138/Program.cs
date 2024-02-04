5d707fcbf2bc303cf525b83d198d8b5c19cac4e4
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-04 13:40:31 -0600

    feat: add 138

---
+++ csharp/Euler138/Program.cs
@@ -0,0 +1,22 @@
+﻿var solutions = new List<long>();
+
+var current = 17L;
+solutions.Add(current);
+var sum = current;
+
+var previous = 1L;
+for (int i = 2; i <= 12; i++)
+{
+    var next = current * 18 - previous;
+    previous = current;
+    current = next;
+    sum += current;
+    solutions.Add(sum);
+}
+
+var smallest = 12L;
+var index = smallest - 1;
+if (index >= solutions.Count)
+    return;
+
+Console.WriteLine(solutions[(int)index]);
