5dc001f7c51c359ab88e65c3101d059b80134c32
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-04 21:27:57 -0600

    feat: add 151

---
+++ csharp/Euler151/Program.cs
@@ -0,0 +1,33 @@
+﻿var result = Math.Round(Evaluate([1, 0, 0, 0, 0]), 6);
+Console.WriteLine(result);
+
+static double Evaluate(List<int> sheets)
+{
+    int numSheets = sheets.Sum();
+
+    var single = 0d;
+    if (numSheets == 1)
+    {
+        if (sheets.Last() == 1)
+            return 0;
+
+        if (sheets.First() == 0)
+            single = 1;
+    }
+
+    for (var i = 0; i < sheets.Count; i++)
+    {
+        if (sheets[i] == 0)
+            continue;
+
+        var next = new List<int>(sheets);
+        next[i]--;
+        for (var j = i + 1; j < next.Count; j++)
+            next[j]++;
+
+        var probability = sheets[i] / (double)numSheets;
+        single += Evaluate(next) * probability;
+    }
+
+    return single;
+}
