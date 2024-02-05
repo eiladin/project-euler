4dec186a4f1d968ce9ee20c1ff3b1f6f70782796
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-04 21:06:33 -0600

    feat: add 148

---
+++ csharp/Euler148/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(Solve(1_000_000_000));
+
+static long Solve(int num)
+{
+    if (num <= 7)
+        return num * (num + 1) / 2;
+    var ans = 7L * 8 / 2;
+    var r = 7;
+    while (r * 7 <= num)
+    {
+        ans *= 7 * 8 / 2;
+        r *= 7;
+    }
+    int level = num / r;
+    ans *= level * (level + 1) / 2;
+    ans += (num / r + 1) * Solve(num % r);
+    return ans;
+}
