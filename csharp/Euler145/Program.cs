2052ae13f06b4385d411531244bf65d0f61212f6
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-04 18:37:22 -0600

    feat: add 145

---
+++ csharp/Euler145/Program.cs
@@ -0,0 +1,11 @@
+﻿var count = Enumerable.Range(2, 8).Sum(Reversable);
+
+Console.WriteLine(count);
+
+static int Reversable(int num) => (num % 4) switch
+{
+    0 => 20 * (int)Math.Pow(30, num / 2 - 1),
+    2 => 20 * (int)Math.Pow(30, num / 2 - 1),
+    3 => 100 * (int)Math.Pow(500, (num - 3) / 4),
+    _ => 0
+};
