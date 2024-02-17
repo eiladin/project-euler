4bf336a009ad77a3ce03498d2f1f46d9aaa9fac6
Khan, Sami <Sami.Khan@tylertech.com>
2024-02-17 09:09:48 -0600

    feat: add 162

---
+++ csharp/Euler162/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+BigInteger result = 0;
+for (int k = 0; k < 16; k++)
+    result += 15 * BigInteger.Pow(16, k)
+            - 15 * BigInteger.Pow(15, k)
+            - 2 * 14 * BigInteger.Pow(15, k)
+            + 13 * BigInteger.Pow(14, k)
+            + 2 * 14 * BigInteger.Pow(14, k)
+            - 13 * BigInteger.Pow(13, k);
+Console.WriteLine(result.ToString("X"));
